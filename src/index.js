f242b680aa4276eb175913086ea42b8f8f7a35f4
hugonicolas <hugoncls@gmail.com>
2018-05-03 10:34:42 +0200

    First files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+class App extends React.Component {
+    
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return(
+
+            <div class="app">
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
