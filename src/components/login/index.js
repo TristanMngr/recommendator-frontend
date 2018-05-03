75c631f25ebca15e3986cd554c0ea67491e995de
clementmosser <clement-mosser@live.fr>
2018-05-03 12:32:29 +0200

    Login

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,18 +1,29 @@
 import React from 'react'
 import style from './login_style.scss'
 import ReactDOM from 'react-dom'
-import {Link} from "react-router-dom"
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
+import LBTransformer from "../../img/Black_transformer.png";
+import RBTransformer from "../../img/right_Black_transformer.png";
+import Logo from "../../img/White_transformer.png";
+
 
 export default class Login extends React.Component {
     render() {
         return(
-
             <div class="login" style={style}>
-                <p>The Recommendator</p>
+
+
+                <div class="space_between">
+                    <img src={LBTransformer} />
+                    <div>
+                        <p>The Recommendator</p>
+                        <img class ="weight" src={Logo} />
+                    </div>
+                    <img src={RBTransformer} />
+                </div>
+
                 <Link to ="/dashboard">Dashboard</Link>
-                <Route path="/dashboard" component={Dashboard}/>
             </div>
-        )
+            )
     }
 }
-ReactDOM.render(<Login/>, document.querySelector('#root'));
