cb8b97393a4dba57c1afa76de7fac00f44887640
clementmosser <clement-mosser@live.fr>
2018-05-03 14:38:19 +0200

    Login fini

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import style from './login_style.scss'
-import ReactDOM from 'react-dom'
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 import LBTransformer from "../../img/Black_transformer.png";
 import RBTransformer from "../../img/right_Black_transformer.png";
@@ -12,17 +11,30 @@ export default class Login extends React.Component {
         return(
             <div class="login" style={style}>
 
-
                 <div class="space_between">
-                    <img src={LBTransformer} />
-                    <div>
-                        <p>The Recommendator</p>
-                        <img class ="weight" src={Logo} />
-                    </div>
-                    <img src={RBTransformer} />
-                </div>
+                    <img class="side" src={LBTransformer} />
 
-                <Link to ="/dashboard">Dashboard</Link>
+                    <div class="down">
+                        <div id="center">
+                            <img class ="weight" src={Logo} />
+                        </div>
+
+                        <div class="space">
+                            <p id="center" class="title">THE RECOMMENDATOR</p>
+                        </div>
+
+                        <form class="column">
+                            <label id="center" class="down">
+                                <input class="input" type="text" name="email" placeholder="E-mail"/>
+                            </label>
+                            <label id="center" class="space">
+                                <input class="input" type="text" name="password" placeholder="Password"/>
+                            </label>
+                            <input id="down" class ="button" type="submit" value="Se connecter" />
+                        </form>
+                    </div>
+                    <img class="side" src={RBTransformer} />
+                </div>
             </div>
             )
     }
